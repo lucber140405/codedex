a5d8c430595cfb212f1afc3d41a1bb09779f3aa5
lucber140405 <122527959+lucber140405@users.noreply.github.com>
2023-02-23 11:48:51 +0100

    Add files via upload

---
+++ examen1.2lmb.py
@@ -0,0 +1,22 @@
+numero = int(input("dime un numero"))
+
+if numero == 1:
+    print("lunes")
+
+if numero == 2:
+    print("martes")
+
+if numero == 3:
+    print("miercoles")
+
+if numero == 4:
+    print("jueves")
+
+if numero == 5:
+    print("viernes")
+
+if numero == 6:
+    print("sabado")
+
+if numero == 7:
+    print("domingo")
